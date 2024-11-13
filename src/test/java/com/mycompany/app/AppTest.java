5364382e243238b500747a870abfcf1ecec1a180
yashkumardubey <ydubey020@gmail.com>
2024-11-13 17:33:29 +0530

    test

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello World! this is yash dubey", app.getMessage());
+        assertEquals("Hello World! this is yash dubey test ", app.getMessage());
     }
 }
