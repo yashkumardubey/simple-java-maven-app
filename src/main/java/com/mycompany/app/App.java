d0ad73d7a5b01c79d9ef41fd4ef827cdd607e8f1
yashkumardubey <ydubey020@gmail.com>
2024-11-26 12:53:22 +0530

    chnages in test file and java fille

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World! this is yash dubey";
+    private static final String MESSAGE = "This is yash dubey";
 
     public App() {}
 
