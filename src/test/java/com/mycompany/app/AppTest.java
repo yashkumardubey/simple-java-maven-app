d0bc82577484eb6d6d694f64d5fc31c6e4e94724
yashkumardubey <ydubey020@gmail.com>
2024-11-29 15:03:38 +0530

    changes in messages

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("This is Remiges Tecnologies and i'm Yash Kumar dubey", app.getMessage());
+        assertEquals("This is Remiges Technologies and i'm Yash Kumar dubey", app.getMessage());
     }
 }
